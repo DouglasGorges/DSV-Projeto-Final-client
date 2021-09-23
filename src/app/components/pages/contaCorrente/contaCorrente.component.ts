149dfc231be89eaac8f21aeddb57efc89cf1913e
Igor Oliveira <igormat4us@gmail.com>
2021-09-23 19:52:21 -0300

    Feito ajustes visuais, melhorias e adicionado menu com opções

---
+++ src/app/components/pages/contaCorrente/contaCorrente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contaCorrente',
+  templateUrl: './contaCorrente.component.html',
+  styleUrls: ['./contaCorrente.component.css']
+})
+export class ContaCorrenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/pages/contaPoupanca/contaPoupanca.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contaPoupanca',
+  templateUrl: './contaPoupanca.component.html',
+  styleUrls: ['./contaPoupanca.component.css']
+})
+export class ContaPoupancaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
